35a9cc484957281f13ced19138d76a800ebcabd2
wangwentao <962533162@qq.com>
2023-03-24 09:02:44 +0800

    测试

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         // 1. 初始化：金库余额、存款账户数量、存款账户列表
 
+        // 测试一下啦
+        System.out.println("GIT TEST");
 
         System.out.println("Hello world!");
     }
