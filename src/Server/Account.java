fcc12b31354c0bd5a7779ab8e59719658382e69a
WWT <962533162@qq.com>
2023-05-10 10:20:44 +0800

    新表userinfo

--- src/Server/Account.java
+++ src/Server/Account.java
@@ -5,6 +5,52 @@ public class Account {
     private String password;
     private String ciphertext;
 
+    private String name;
+    private String sex;
+    private String idnumber;
+    private String email;
+    private Double balance;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getIdnumber() {
+        return idnumber;
+    }
+
+    public void setIdnumber(String idnumber) {
+        this.idnumber = idnumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
     public String getUser() {
         return user;
     }
--- src/Server/JDBC/BaseDao.java
+++ src/Server/JDBC/BaseDao.java
@@ -12,14 +12,24 @@ public class BaseDao {
     static {
         conn = DBCNUnit.getConnection();
     }
-    public void addAccount(String user, String password, String ciphertext) throws SQLException {
+    public void addAccount(Account account) throws SQLException {
         String sql = "INSERT INTO account values(?,?,?)";
         PreparedStatement ptmt = conn.prepareStatement(sql);
-        ptmt.setString(1, user);
-        ptmt.setString(2, password);
-        ptmt.setString(3, ciphertext);
+        ptmt.setString(1, account.getUser());
+        ptmt.setString(2, account.getPassword());
+        ptmt.setString(3, account.getCiphertext());
         ptmt.execute();
+        String sql1 = "INSERT INTO userinfo values(?,?,?,?,?,?)";
+        PreparedStatement ptmt1 = conn.prepareStatement(sql1);
+        ptmt1.setString(1, account.getUser());
+        ptmt1.setString(2, account.getName());
+        ptmt1.setString(3, account.getSex());
+        ptmt1.setString(4, account.getIdnumber());
+        ptmt1.setString(5, account.getEmail());
+        ptmt1.setDouble(6, account.getBalance());
+        ptmt1.execute();
     }
+
     public List<Account> queryAllAccount() throws SQLException {
         Statement stmt = conn.createStatement();
         ResultSet resultSet = stmt.executeQuery("select * from account");
@@ -30,10 +40,16 @@ public class BaseDao {
             account.setUser(resultSet.getString("account"));
             account.setPassword(resultSet.getString("password"));
             account.setCiphertext(resultSet.getString("ciphertext"));
-
             list.add(account);
         }
         return list;
+    }
+    public void Withdraw(double money, String account) throws SQLException {
+        String sql = "UPDATE account SET balance=balance-? WHICH account=?";
+        PreparedStatement ptmt = conn.prepareStatement(sql);
+        ptmt.setDouble(1, money);
+        ptmt.setString(2, account);
+        ptmt.execute();
     }
 
 //    public static void main(String[] args){
